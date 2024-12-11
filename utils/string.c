006e71f009785f478d3e1cfe3c9f4e87ded8ab70
yekuuun <yekuuun@proton.me>
2024-12-11 20:20:23 +0100

    UPDATE : string.c update => adding more methods

--- utils/string.c
+++ utils/string.c
@@ -12,3 +12,41 @@ SIZE_T CustomStrLenA(LPCSTR str){
     }
     return str - ptrStr;
 }
+
+/**
+ * Contains a string.
+ */
+BOOL StringContains(wchar_t* haystack, wchar_t* needle){
+    while(*haystack && (*haystack == *needle)){
+        haystack++;
+        needle++;
+    }
+
+    return (*haystack == *needle);
+}
+
+/**
+ * Compare 2 given LCSTR strings (use other types if you want)
+ */
+BOOL CompareStringsA(LPCSTR str1, LPCSTR str2){
+    if(str1 == NULL || str2 == NULL)
+    {
+        return FALSE;
+    }
+
+    if(CustomStrLenA(str1) != CustomStrLenA(str2))
+    {
+        return FALSE;
+    }
+
+    while(*str1 != '\0' && *str2 != '\0')
+    {
+        if(*str1 != *str2)
+        {
+            return FALSE;
+        }
+        str2++;
+        str1++;
+    }
+    return TRUE;
+}
